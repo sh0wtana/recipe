22951c1928e961387fcda1f5a5f366deda12fa2e
shtan14 <wrzn.inw.snun@gmail.com>
2026-07-26 10:05:18 +0900

    install annotaterb

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,5 +1,19 @@
 require "test_helper"
 
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  email_address   :string           not null
+#  password_digest :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email_address  (email_address) UNIQUE
+#
 class UserTest < ActiveSupport::TestCase
   test "downcases and strips email_address" do
     user = User.new(email_address: " DOWNCASED@EXAMPLE.COM ")
