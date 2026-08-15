a10a230bfed3ceebe23cbeb51afecad7a3bee3a2
shtan14 <wrzn.inw.snun@gmail.com>
2026-08-16 08:11:12 +0900

    Add regression coverage for forms outside the recipe editor
    - Recipe search box: Enter still searches
    - Sign-in: Enter still submits

---
+++ test/system/sessions_test.rb
@@ -0,0 +1,13 @@
+require "application_system_test_case"
+
+class SessionsTest < ApplicationSystemTestCase
+  test "Enter in the password field still signs in" do
+    visit new_session_path
+
+    fill_in I18n.t("sessions.new.email_placeholder"), with: users(:one).email_address
+    fill_in I18n.t("sessions.new.password_placeholder"), with: "password"
+    find_field(I18n.t("sessions.new.password_placeholder")).send_keys(:enter)
+
+    assert_current_path root_path
+  end
+end
